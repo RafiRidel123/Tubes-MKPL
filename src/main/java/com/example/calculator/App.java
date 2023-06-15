d76e14018c0171ad3e2e2f8dd087f747f5c8f73a
RafiAR <56111747+RafiRidel123@users.noreply.github.com>
2023-06-15 15:31:31 +0700

    Update App.java

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -6,13 +6,13 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan angka pertama: ");
+        System.out.print("Inputkan angka pertama: ");
         double angka1 = input.nextDouble();
 
-        System.out.print("Masukkan operator (+, -, *, /): ");
+        System.out.print("Inputkan operator (+, -, *, /): ");
         char operator = input.next().charAt(0);
 
-        System.out.print("Masukkan angka kedua: ");
+        System.out.print("Inputkan angka kedua: ");
         double angka2 = input.nextDouble();
 
         double hasil;
